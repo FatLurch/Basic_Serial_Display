aafe217d899019d0f85dafb858c94fe88cc61346
Erik (Lurch) <fat.lurch@gmail.com>
2021-10-30 17:10:54 -0700

    Address control characters

--- Basic_Serial_Display.ino
+++ Basic_Serial_Display.ino
@@ -1,4 +1,8 @@
 /*
+ * Fat_Lurch - 2021-10-29
+ * 
+ * Updated - 2021-10-30: Added handling to control characters so they are not displayed
+ * 
  * This is a very simple sketch that will display a single line of text (only) on a standard 16x2 LCD I2C display
  * 
  * Based on the original example from johnrickman: https://github.com/johnrickman/LiquidCrystal_I2C/blob/master/examples/SerialDisplay/SerialDisplay.pde
@@ -13,25 +17,35 @@
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 (default) for a 16 chars and 2 line display
 
+char charBuf;
+
 void setup()
 {
-  lcd.init();                      // initialize the lcd 
-  lcd.backlight();
+  lcd.init();               // initialize the LCD
+  lcd.backlight();          //Turn on LCD backlight
   Serial.begin(9600);
 }
 
 void loop()
 {
   // when characters arrive over the serial port...
-  if (Serial.available()) {
+  if (Serial.available()) 
+  {
     // wait a bit for the entire message to arrive
     delay(100);
     // clear the screen
     lcd.clear();
     // read all the available characters
-    while (Serial.available() > 0) {
-      // display each character to the LCD
-      lcd.write(Serial.read());
+    while (Serial.available() > 0) 
+    {
+      // Read the character to a buffer
+      charBuf = char(Serial.read());
+      
+      // Skip control characters like \r and \n (aka CR and LF)
+      if (!isControl(charBuf)) 
+      {
+        lcd.write(charBuf);
+      }
     }
   }
 }
